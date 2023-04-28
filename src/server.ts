ce2a437ea46c0cabab97eeb22f523ddb89d4588a
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-04-28 20:56:54 -0300

    add body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
   res.send('Hello World');
