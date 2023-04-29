fd68ca69ad1f98b66519d1e548e69cc2acf2fe43
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-04-28 22:01:42 -0300

    refactor GenreController

--- src/controller/GenreController.ts
+++ src/controller/GenreController.ts
@@ -4,39 +4,41 @@ import { Genre } from '../model/Genre';
 import { GenreService } from '../service/GenreService';
 
 export class GenreController {
-  private service: GenreService;
-
-  constructor() {
-    this.service = new GenreService();
-  }
-
   public async createGenre(req: Request, res: Response) {
-    let responseDTO: ResponseDTO<Genre> = {
-      data: [],
-      errors: ["Genre's name is not defined."],
-      success: false,
-    };
-
-    if (!req.body.name) return res.status(400).json(responseDTO);
-
-    const genre = await this.service.createGenre(req.body.name);
-
-    if (genre == null) {
-      responseDTO = {
+    try {
+      let responseDTO: ResponseDTO<Genre> = {
         data: [],
-        errors: ['This genre already exists.'],
+        errors: ["Genre's name is not defined."],
         success: false,
       };
 
-      return res.status(400).json(responseDTO);
+      if (!req.body.name) return res.status(400).json(responseDTO);
+
+      const service = new GenreService();
+
+      const genre = await service.createGenre(req.body.name);
+
+      if (genre == null) {
+        responseDTO = {
+          data: [],
+          errors: ['This genre already exists.'],
+          success: false,
+        };
+
+        return res.status(400).json(responseDTO);
+      }
+
+      responseDTO = {
+        data: [genre],
+        success: true,
+        errors: [],
+      };
+
+      return res.status(200).json(responseDTO);
+    } catch (error: any) {
+      console.error(error.stack);
+
+      return res.status(500).json({ error: 'Internal server error.' });
     }
-
-    responseDTO = {
-      data: [genre],
-      success: true,
-      errors: [],
-    };
-
-    return res.status(200).json(responseDTO);
   }
 }
