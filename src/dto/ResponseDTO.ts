4c345048119e2121b20f0f4fcd577b8337854349
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-04-29 13:12:39 -0300

    refactor ResponseDTO

--- src/dto/ResponseDTO.ts
+++ src/dto/ResponseDTO.ts
@@ -1,4 +1,6 @@
-export interface ResponseDTO<T> {
+import { type BaseModel } from '../model/BaseModel'
+
+export interface ResponseDTO<T extends BaseModel> {
   errors: string[]
   success: boolean
   data: T[]
