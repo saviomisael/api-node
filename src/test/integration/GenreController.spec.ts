82df809f460797b99c36d17c99b299abcc9512e8
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-02 21:53:33 -0300

    add test should create a genre

--- src/test/integration/GenreController.spec.ts
+++ src/test/integration/GenreController.spec.ts
@@ -12,7 +12,7 @@ describe('POST /api/v1/genres', () => {
     await connection.execute('DELETE FROM genres')
   })
 
-  it('should returns a bad request response when name is not provided', async () => {
+  it('should return a bad request response when name is not provided', async () => {
     const response = await chai.request(app)
       .post('/api/v1/genres')
       .send({})
@@ -23,7 +23,7 @@ describe('POST /api/v1/genres', () => {
     chai.expect(response.body.success).to.be.false
   })
 
-  it('should returns a bad request when genre already exists.', async () => {
+  it('should return a bad request when genre already exists.', async () => {
     const firstResponse = await chai.request(app).post('/api/v1/genres').send({
       name: 'action'
     })
@@ -39,5 +39,17 @@ describe('POST /api/v1/genres', () => {
     chai.expect(secondResponse.body.errors).to.have.length(1)
     chai.expect(secondResponse.body.errors[0]).to.be.equal('This genre already exists.')
     chai.expect(secondResponse.body.data).to.have.length(0)
+  })
+
+  it('should create a genre', async () => {
+    const response = await chai.request(app).post('/api/v1/genres').send({
+      name: 'action'
+    })
+
+    chai.expect(response).to.have.status(201)
+    chai.expect(response.body.success).to.be.true
+    chai.expect(response.body.errors).to.have.length(0)
+    chai.expect(response.body.data).to.have.length(1)
+    chai.expect(response.body.data[0].name).to.be.equal('action')
   })
 })
