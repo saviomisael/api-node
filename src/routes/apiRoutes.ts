65957bc03e35351cc250ce453610912c3f2529da
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-06 16:44:34 -0300

    update genreRouter

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -1,6 +1,7 @@
 export const apiRoutes = {
   genres: {
     create: '/api/v1/genres',
-    getAll: '/api/v1/genres'
+    getAll: '/api/v1/genres',
+    deleteById: '/api/v1/genres/:id'
   }
 }
--- src/routes/genreRouter.ts
+++ src/routes/genreRouter.ts
@@ -7,5 +7,6 @@ const controller = new GenreController()
 
 genreRouter.post(apiRoutes.genres.create, controller.createGenre.bind(controller))
 genreRouter.get(apiRoutes.genres.getAll, controller.getAllGenres.bind(controller))
+genreRouter.delete(apiRoutes.genres.deleteById, controller.deleteGenre.bind(controller))
 
 export default genreRouter
