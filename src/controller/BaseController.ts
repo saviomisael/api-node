6fca0eededcde1ed99ac388322ffb4471aa8deb8
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-10 21:08:03 -0300

    refactor BaseController

--- src/controller/BaseController.ts
+++ src/controller/BaseController.ts
@@ -2,23 +2,23 @@ import { type Response } from 'express'
 import { badRequest, created, noContent, notFound, ok } from '../util/http-helper'
 
 export abstract class BaseController {
-  badRequest<T>(res: Response, body: T): Response {
+  protected badRequest<T>(res: Response, body: T): Response {
     return badRequest(res, body)
   }
 
-  created<T>(res: Response, body: T): Response {
+  protected created<T>(res: Response, body: T): Response {
     return created(res, body)
   }
 
-  ok<T>(res: Response, body: T): Response {
+  protected ok<T>(res: Response, body: T): Response {
     return ok(res, body)
   }
 
-  noContent (res: Response): Response {
+  protected noContent (res: Response): Response {
     return noContent(res)
   }
 
-  notFound<T>(res: Response, body: T): Response {
+  protected notFound<T>(res: Response, body: T): Response {
     return notFound(res, body)
   }
 }
