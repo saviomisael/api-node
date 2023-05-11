1bfe9e2becf49655139a0fc8c773270a5acd6156
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-10 21:24:14 -0300

    pass test should return bad request when platform name have less then 3 characters.

---
+++ src/test/integration/PlatformController.spec.ts
@@ -0,0 +1,27 @@
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+import { DBConnection } from '../../data/DBConnection'
+import { apiRoutes } from '../../routes/apiRoutes'
+import app from '../../server'
+
+chai.use(chaiHttp)
+
+const clearData = async (): Promise<void> => {
+  const connection = await DBConnection.getConnection()
+
+  await connection.execute('DELETE FROM platforms')
+}
+
+describe('POST /api/v1/platforms', () => {
+  afterEach(async () => {
+    await clearData()
+  })
+
+  it('should return bad request when platform name have less then 3 characters.', async () => {
+    const response = await chai.request(app).post(apiRoutes.platforms.create).send({ name: 'ab' })
+
+    chai.expect(response).to.have.status(400)
+    chai.expect(response.body.errors).to.have.length(1)
+    chai.expect(response.body.errors[0]).to.be.equal('The platform name must have at least 3 characters.')
+  })
+})
