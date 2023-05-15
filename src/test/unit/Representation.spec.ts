75e28d08ee612245a594b5b131e97bd30777bce8
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-15 19:40:58 -0300

    pass test should add a custom link

--- src/test/unit/Representation.spec.ts
+++ src/test/unit/Representation.spec.ts
@@ -14,4 +14,17 @@ describe('Representation', () => {
       .be
       .equal('/api/v1/genres')
   })
+
+  it('should add a custom link', () => {
+    const representation = new Representation()
+      .addLink('list', '/api/v1/genres')
+      .getRepresentation()
+
+    chai.expect(representation.getLinks()).to.have.length(1)
+    chai.expect(representation.getLinks()[0].getRel()).to.be.equal('list')
+    chai.expect(representation.getLinks()[0].getHref())
+      .to
+      .be
+      .equal('/api/v1/genres')
+  })
 })
