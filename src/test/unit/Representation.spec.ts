8f96f2f7e4554d68b9dc91a92a0522076f2e57cc
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-15 20:11:36 -0300

    pass test should add a self link to a custom class

--- src/test/unit/Representation.spec.ts
+++ src/test/unit/Representation.spec.ts
@@ -1,6 +1,12 @@
 import chai from 'chai'
 import { Representation } from '../../hateoas/Representation'
 
+class MockClass extends Representation {
+  constructor (public readonly mockProperty: string) {
+    super()
+  }
+}
+
 describe('Representation', () => {
   it('should create a representation with self link', () => {
     const representation = new Representation()
@@ -15,7 +21,7 @@ describe('Representation', () => {
       .equal('/api/v1/genres')
   })
 
-  it('should add a custom link', () => {
+  it('should add a self link to a custom class', () => {
     const representation = new Representation()
       .addLink('list', '/api/v1/genres')
       .getRepresentation()
@@ -26,5 +32,13 @@ describe('Representation', () => {
       .to
       .be
       .equal('/api/v1/genres')
+  })
+
+  it('should have _links inside a custom class', () => {
+    const mock = new MockClass('mock').addSelfLink('/api/v1/mocks')
+
+    chai.expect(mock.getLinks()).to.have.length(1)
+    chai.expect(mock.getLinks()[0].getRel()).to.be.equal('self')
+    chai.expect(mock.getLinks()[0].getHref()).to.be.equal('/api/v1/mocks')
   })
 })
