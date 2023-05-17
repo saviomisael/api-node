06d53adb2571914e7a20a4a2bdc1e94b2930d4ac
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-17 20:52:05 -0300

    add deletePlatform method to controller

--- src/controller/PlatformController.ts
+++ src/controller/PlatformController.ts
@@ -1,6 +1,6 @@
 import { validate } from 'class-validator'
 import { type Request, type Response } from 'express'
-import { CreatePlatformDTO, type ResponseDTO } from '../dto'
+import { CreatePlatformDTO, DeletePlatformDTO, type ResponseDTO } from '../dto'
 import { type Platform } from '../model/Platform'
 import { PlatformService } from '../service/PlatformService'
 import { BaseController } from './BaseController'
@@ -44,5 +44,41 @@ export class PlatformController extends BaseController {
     }
 
     return this.created(res, response)
+  }
+
+  async deletePlatform (req: Request, res: Response): Promise<Response> {
+    let response: ResponseDTO<Platform> = {
+      data: [],
+      errors: [],
+      success: false
+    }
+
+    const dto = new DeletePlatformDTO(req.params.id)
+
+    const errors = await validate(dto)
+
+    if (errors.length > 0 && errors[0].constraints != null) {
+      response = {
+        data: [],
+        errors: [...Object.values(errors[0].constraints)],
+        success: false
+      }
+
+      return this.badRequest(res, response)
+    }
+
+    const isDeleted = await this.service.deletePlatformById(dto.id)
+
+    if (!isDeleted) {
+      response = {
+        data: [],
+        success: false,
+        errors: ['This platform do not exists.']
+      }
+
+      return this.notFound(res, response)
+    }
+
+    return this.noContent(res)
   }
 }
