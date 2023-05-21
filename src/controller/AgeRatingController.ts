6247ebf5c7ac58637bf5d7b65b31c3e19c72612f
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-20 22:11:11 -0300

    refactor AgeRatingController

--- src/controller/AgeRatingController.ts
+++ src/controller/AgeRatingController.ts
@@ -1,13 +1,21 @@
 import { type Request, type Response } from 'express'
+import { type ResponseDTO } from '../dto'
+import { type AgeRating } from '../model/AgeRating'
 import { AgeRatingService } from '../service/AgeRatingService'
 import { BaseController } from './BaseController'
 
 export class AgeRatingController extends BaseController {
   private readonly service = new AgeRatingService()
 
-  async getAll (req: Request, res: Response): Promise<Response> {
+  async getAll (_: Request, res: Response): Promise<Response> {
     const ageRatings = await this.service.getAllAgeRatings()
 
-    return this.ok(res, ageRatings)
+    const response: ResponseDTO<AgeRating> = {
+      data: [...ageRatings],
+      success: true,
+      errors: []
+    }
+
+    return this.ok(res, response)
   }
 }
