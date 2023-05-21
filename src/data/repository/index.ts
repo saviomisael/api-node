682b322b023b93f14e9775a20cd1a7e36b321948
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-20 21:12:35 -0300

    add index file for repositories

---
+++ src/data/repository/index.ts
@@ -0,0 +1,6 @@
+export { AgeRatingRepository } from './AgeRatingRepository'
+export { GenreRepository } from './GenreRepository'
+export type { IAgeRatingRepository } from './IAgeRatingRepository'
+export type { IGenreRepository } from './IGenreRepository'
+export type { IPlatformRepository } from './IPlatformRepository'
+export { PlatformRepository } from './PlatformRepository'
