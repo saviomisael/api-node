b2fe438e2cdcfc89db661aa1fb2329274a9728fa
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-05-23 23:18:48 -0300

    refactor AgeRatingController

--- src/presentation/controllers/AgeRatingController.ts
+++ src/presentation/controllers/AgeRatingController.ts
@@ -1,20 +1,41 @@
 import { ListAgeRatingService } from '$/application/services/ListAgeRatingService'
 import { type AgeRating } from '$/domain/entities'
+import { AgeRatingCacheService } from '$/infrastructure/services/AgeRatingCacheService'
+import { type CacheService } from '$/infrastructure/services/CacheService'
 import { BaseController } from '$/presentation/controllers/BaseController'
 import { type ResponseDTO } from '$/presentation/dto'
 import { type Request, type Response } from 'express'
 
 export class AgeRatingController extends BaseController {
-  private readonly service = new ListAgeRatingService()
+  private readonly listService = new ListAgeRatingService()
+  private readonly cacheService: CacheService<AgeRating[]> = new AgeRatingCacheService()
 
   async getAll (_: Request, res: Response): Promise<Response> {
-    const ageRatings = await this.service.getAllAgeRatings()
+    const results = await this.cacheService.getData()
+    let ageRatings: AgeRating[]
+    let response: ResponseDTO<AgeRating>
 
-    const response: ResponseDTO<AgeRating> = {
+    if (results != null) {
+      ageRatings = results
+
+      response = {
+        data: [...results],
+        success: true,
+        errors: []
+      }
+
+      return this.ok(res, response)
+    }
+
+    ageRatings = await this.listService.getAllAgeRatings()
+
+    response = {
       data: [...ageRatings],
       success: true,
       errors: []
     }
+
+    await this.cacheService.setData(ageRatings)
 
     return this.ok(res, response)
   }
