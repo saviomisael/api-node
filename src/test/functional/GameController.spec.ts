5569291fe6d73508cf30987e99db1c5948daa4b8
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-08 11:16:47 -0300

    pass test should return a bad request when platforms has duplicates

--- src/test/functional/GameController.spec.ts
+++ src/test/functional/GameController.spec.ts
@@ -23,4 +23,22 @@ describe('POST /api/v1/games', () => {
     chai.expect(response.body.success).to.be.false
     chai.expect(response.body.errors).to.have.length(1)
   })
+
+  it('should return a bad request when platforms has duplicates', async () => {
+    const requestData = {
+      ageRatingId: '123',
+      description: '123',
+      genres: ['123', '1234'],
+      platforms: ['123', '123'],
+      name: 'game',
+      price: 0,
+      releaseDate: '2020-01-01'
+    }
+    const response = await chai.request(app).post(apiRoutes.games.create).send(requestData)
+
+    chai.expect(response).to.have.status(400)
+    chai.expect(response.body.data).to.have.length(0)
+    chai.expect(response.body.success).to.be.false
+    chai.expect(response.body.errors).to.have.length(1)
+  })
 })
