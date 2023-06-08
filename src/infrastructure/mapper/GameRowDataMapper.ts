9cc9dcaf061b5d4615c8ceadee15f978a2c309b9
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-08 17:33:41 -0300

    pass test should map a row data to entity

---
+++ src/infrastructure/mapper/GameRowDataMapper.ts
@@ -0,0 +1,32 @@
+import { AgeRating, Game, Genre, Platform } from '$/domain/entities'
+
+export class GameRowDataMapper {
+  private constructor () {}
+
+  static toEntity (row: any): Game {
+    const age = new AgeRating(row.age, row.age_rating_description)
+    age.id = row.age_rating_id
+    const game = new Game(row.game_name, row.game_price, row.game_description, new Date(row.releaseDate), age)
+    game.id = row.game_id
+
+    for (let index = 0; index < row.genre_ids.split(',').length; index++) {
+      const genreId = row.genre_ids.split(',')[index]
+      const genreName = row.genre_names.split(',')[index]
+      const genre = new Genre(genreName)
+      genre.id = genreId
+      console.log(genre)
+      game.addGenre(genre)
+    }
+
+    for (let index = 0; index < row.platform_ids.split(',').length; index++) {
+      const platformId = row.platform_ids.split(',')[index]
+      const platformName = row.platform_names.split(',')[index]
+      const platform = new Platform(platformName)
+      platform.id = platformId
+      console.log(platform)
+      game.addPlatform(platform)
+    }
+
+    return game
+  }
+}
