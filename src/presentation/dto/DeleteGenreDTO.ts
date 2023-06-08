3f03a425f1614a8911f61e821e3f008911bd3460
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-08 09:21:37 -0300

    refactor DeleteGenreDTO

--- src/presentation/dto/DeleteGenreDTO.ts
+++ src/presentation/dto/DeleteGenreDTO.ts
@@ -2,7 +2,7 @@ import { Length } from 'class-validator'
 
 export class DeleteGenreDTO {
   @Length(36, 36, {
-    message: 'Id must have 36 characters.'
+    message: 'O id precisa ter 36 caracteres.'
   })
     id: string
 
