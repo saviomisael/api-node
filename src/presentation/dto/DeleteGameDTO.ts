cb34178a7a0544c0c21cc17fa07a9297f44c51bc
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-09 19:18:09 -0300

    add DeleteGameDTO

---
+++ src/presentation/dto/DeleteGameDTO.ts
@@ -0,0 +1,9 @@
+import { Matches } from 'class-validator'
+import { uuidRegex } from '../constants'
+
+export class DeleteGameDTO {
+  @Matches(uuidRegex, {
+    message: 'O id do jogo deve ser um uuid válido.'
+  })
+    id!: string
+}
