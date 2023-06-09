a289465f5b044fea35e85e13ae16d54746524179
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-09 13:03:28 -0300

    add GetGameDTO

---
+++ src/presentation/dto/GetGameDTO.ts
@@ -0,0 +1,8 @@
+import { Matches } from 'class-validator'
+
+export class GetGameDTO {
+  @Matches(/^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$/, {
+    message: 'O id do jogo deve ser um uuid válido.'
+  })
+    id!: string
+}
