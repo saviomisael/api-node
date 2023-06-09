d353c977ad73863b87a7d373c0ee8f9df1b50bae
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-09 16:59:58 -0300

    add fromUpdateGameDtoToEntity

--- src/presentation/mapper/GameMapper.ts
+++ src/presentation/mapper/GameMapper.ts
@@ -1,6 +1,7 @@
 import { AgeRating, Game, Genre, Platform } from '$/domain/entities'
 import { type CreateGameDTO } from '../dto/CreateGameDTO'
 import { GameResponseDTO } from '../dto/GameResponseDTO'
+import { type UpdateGameDTO } from '../dto/UpdateGameDTO'
 
 export class GameMapper {
   private constructor () {}
@@ -10,6 +11,28 @@ export class GameMapper {
     ageRating.id = dto.ageRatingId
 
     const game = new Game(dto.name, dto.price, dto.description, new Date(dto.releaseDate), ageRating)
+
+    for (const genreId of dto.genres) {
+      const genre = new Genre('')
+      genre.id = genreId
+      game.addGenre(genre)
+    }
+
+    for (const platformId of dto.platforms) {
+      const platform = new Platform('')
+      platform.id = platformId
+      game.addPlatform(platform)
+    }
+
+    return game
+  }
+
+  static fromUpdateGameDtoToEntity (dto: UpdateGameDTO): Game {
+    const ageRating = new AgeRating('', '')
+    ageRating.id = dto.ageRatingId
+
+    const game = new Game(dto.name, dto.price, dto.description, new Date(dto.releaseDate), ageRating)
+    game.id = dto.id
 
     for (const genreId of dto.genres) {
       const genre = new Genre('')
