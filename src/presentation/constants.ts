7658e4125f8e7e0170dad1abaad964c9d03e1214
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-10 20:42:33 -0300

    add minPages constant

--- src/presentation/constants.ts
+++ src/presentation/constants.ts
@@ -1,3 +1,5 @@
 export const uuidRegex = /^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$/
 
 export const dateRegex = /^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$/
+
+export const minPages = 1
