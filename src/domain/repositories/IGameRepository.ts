770e3541f0ec62b784b8df85d897bb2149b851fa
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-10 20:06:07 -0300

    add getAll and getMaxPages for GameRepository

--- src/domain/repositories/IGameRepository.ts
+++ src/domain/repositories/IGameRepository.ts
@@ -5,4 +5,6 @@ export interface IGameRepository {
   getById: (gameId: string) => Promise<Game | null>
   updateGame: (newGame: Game) => Promise<void>
   deleteGameById: (gameId: string) => Promise<void>
+  getAll: (page: number, sortType: 'releaseDate', sortOrder: 'ASC' | 'DESC') => Promise<Game[]>
+  getMaxPages: () => Promise<number>
 }
--- src/infrastructure/repositories/GameRepository.ts
+++ src/infrastructure/repositories/GameRepository.ts
@@ -2,6 +2,7 @@ import { type Game } from '$/domain/entities'
 import { type IGameRepository } from '$/domain/repositories'
 import { type Connection } from 'mysql2/promise'
 import { DBConnection } from '../DBConnection'
+import { maxGamesPerPage } from '../constants'
 import { GameRowDataMapper } from '../mapper/GameRowDataMapper'
 import { type GameRowData } from '../row-data/GameRowData'
 
@@ -123,5 +124,53 @@ export class GameRepository implements IGameRepository {
     pipeline.push(this.connection.execute('DELETE FROM games WHERE id = ?', [gameId]))
 
     await Promise.all([...pipeline])
+  }
+
+  async getAll (page: number, sortType: 'releaseDate', sortOrder: 'ASC' | 'DESC'): Promise<Game[]> {
+    this.connection = await DBConnection.getConnection()
+
+    const orders = {
+      releaseDate: 'g.releaseDate'
+    }
+
+    const query = `
+      SELECT g.id AS game_id,
+      g.name AS game_name,
+      g.description AS game_description,
+      g.price AS game_price,
+      g.releaseDate,
+      ar.id AS age_rating_id,
+      ar.age,
+      ar.description AS age_rating_description,
+      GROUP_CONCAT(DISTINCT gr.id) AS genre_ids,
+      GROUP_CONCAT(DISTINCT gr.name) AS genre_names,
+      GROUP_CONCAT(DISTINCT p.id) AS platform_ids,
+      GROUP_CONCAT(DISTINCT p.name) AS platform_names
+      FROM games AS g
+      JOIN ageRatings AS ar ON g.fkAgeRating = ar.id
+      JOIN games_genres AS gg ON gg.fk_game = g.id
+      JOIN genres AS gr ON gg.fk_genre = gr.id
+      JOIN games_platforms AS gp ON gp.fk_game = g.id
+      JOIN platforms AS p ON gp.fk_platform = p.id
+      GROUP BY g.id
+      ORDER BY ${orders[sortType]} ${sortOrder}
+      LIMIT 9 OFFSET ${page * maxGamesPerPage}
+    `
+
+    const result = await this.connection.execute(query)
+
+    const rows = result[0] as GameRowData[]
+
+    return rows.map(x => GameRowDataMapper.toEntity(x))
+  }
+
+  async getMaxPages (): Promise<number> {
+    this.connection = await DBConnection.getConnection()
+
+    const result = await this.connection.execute('SELECT COUNT(id) AS numGames FROM games')
+
+    const rows = result[0] as any[]
+
+    return Math.ceil(rows[0].numGames as number / maxGamesPerPage)
   }
 }
