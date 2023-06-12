2c6eb843cd09ce18130fb6f48f132e02a85a550e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-11 21:48:56 -0300

    update collation for db

--- sql/database.sql
+++ sql/database.sql
@@ -1,22 +1,27 @@
 USE gamesdb;
 
+ALTER DATABASE gamesdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE TABLE genres (
   id VARCHAR(36) PRIMARY KEY NOT NULL,
   name VARCHAR(256) NOT NULL UNIQUE,
   FULLTEXT idx_ge_name (name)
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE platforms (
   id VARCHAR(36) PRIMARY KEY NOT NULL,
   name VARCHAR(256) NOT NULL UNIQUE,
   FULLTEXT idx_p_name (name)
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE ageRatings (
   id VARCHAR(36) PRIMARY KEY NOT NULL,
   age VARCHAR(3) UNIQUE NOT NULL,
   description VARCHAR(256) NOT NULL
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE games (
   id VARCHAR(36) PRIMARY KEY NOT NULL,
@@ -27,7 +32,8 @@ CREATE TABLE games (
   fkAgeRating VARCHAR(36) NOT NULL,
   FOREIGN KEY (fkAgeRating) REFERENCES ageRatings(id),
   FULLTEXT idx_g_name (name)
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE games_genres (
   fk_genre VARCHAR(36) NOT NULL,
@@ -35,7 +41,8 @@ CREATE TABLE games_genres (
   FOREIGN KEY (fk_genre) REFERENCES genres(id),
   FOREIGN KEY (fk_game) REFERENCES games(id),
   PRIMARY KEY (fk_genre, fk_game)
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE games_platforms (
   fk_platform VARCHAR(36) NOT NULL,
@@ -43,4 +50,5 @@ CREATE TABLE games_platforms (
   FOREIGN KEY (fk_platform) REFERENCES platforms(id),
   FOREIGN KEY (fk_game) REFERENCES games(id),
   PRIMARY KEY (fk_platform, fk_game)
-);
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
