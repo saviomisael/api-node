a37fb1df55bf4aad87b6926c54736e1d22b8a005
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-15 12:56:00 -0300

    pass test should return 1 hour a.m. when old date is 12 p.m.

--- src/test/unit/domain/helpers/addHoursHelper.spec.ts
+++ src/test/unit/domain/helpers/addHoursHelper.spec.ts
@@ -14,4 +14,17 @@ describe('addHoursHelper', () => {
     chai.expect(date.getSeconds()).to.be.equal(dateAfter1Hour.getSeconds())
     chai.expect(date.getMilliseconds()).to.be.equal(dateAfter1Hour.getMilliseconds())
   })
+
+  it('should return 1 hour a.m. when old date is 12 p.m.', () => {
+    const date = new Date(2020, 0, 1, 24, 30, 30)
+    const dateAfter1Hour = addHoursHelper(date, 1)
+
+    chai.expect(date.getFullYear()).to.be.equal(dateAfter1Hour.getFullYear())
+    chai.expect(date.getMonth()).to.be.equal(dateAfter1Hour.getMonth())
+    chai.expect(date.getDate()).to.be.equal(dateAfter1Hour.getDate())
+    chai.expect(date.getHours() + 1).to.be.equal(dateAfter1Hour.getHours())
+    chai.expect(date.getMinutes()).to.be.equal(dateAfter1Hour.getMinutes())
+    chai.expect(date.getSeconds()).to.be.equal(dateAfter1Hour.getSeconds())
+    chai.expect(date.getMilliseconds()).to.be.equal(dateAfter1Hour.getMilliseconds())
+  })
 })
