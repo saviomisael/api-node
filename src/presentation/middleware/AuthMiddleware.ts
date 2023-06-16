0eea9f7ad1becef2361ac1669cf855e0c8e1a1df
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-16 14:06:16 -0300

    add AuthMiddleware

---
+++ src/presentation/middleware/AuthMiddleware.ts
@@ -0,0 +1,55 @@
+import { JWTGenerator } from '$/infrastructure/JWTGenerator'
+import { type NextFunction, type Response } from 'express'
+import { JsonWebTokenError, NotBeforeError, TokenExpiredError } from 'jsonwebtoken'
+import { BaseController } from '../controllers/BaseController'
+import { type ResponseDTO } from '../dto'
+import { type JWTRequest } from '../requests/JWTRequest'
+
+export class AuthMiddleware extends BaseController {
+  async isAuthenticaded(req: JWTRequest, res: Response, next: NextFunction): Promise<Response | undefined> {
+    const authorizationHeader = req.headers.authorization
+
+    let response: ResponseDTO<any> = {
+      data: [],
+      errors: ['Authorization header não enviado.'],
+      success: false
+    }
+
+    if (authorizationHeader === undefined) {
+      return this.badRequest(res, response)
+    }
+
+    const token = authorizationHeader.split(' ')[1]
+
+    const generator = new JWTGenerator()
+
+    try {
+      const payload = await generator.verifyToken(token)
+
+      req.payload = payload
+      next()
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        response = {
+          data: [],
+          errors: ['Token expirou.'],
+          success: false
+        }
+
+        return this.badRequest(res, response)
+      }
+
+      if (error instanceof JsonWebTokenError || error instanceof NotBeforeError) {
+        response = {
+          data: [],
+          errors: ['Token inválido.'],
+          success: false
+        }
+
+        return this.badRequest(res, response)
+      }
+
+      throw error
+    }
+  }
+}
