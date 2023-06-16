60a67be19312dd08829029dc30dccafae88b3f6e
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-15 21:06:16 -0300

    pass test should return invalid token when token provided is invalid

--- src/test/unit/infrastructure/JWTGenerator.spec.ts
+++ src/test/unit/infrastructure/JWTGenerator.spec.ts
@@ -1,6 +1,7 @@
 import { JWTGenerator } from '$/infrastructure/JWTGenerator'
 import chai from 'chai'
 import dotenv from 'dotenv'
+import { JsonWebTokenError } from 'jsonwebtoken'
 
 dotenv.config()
 
@@ -14,5 +15,18 @@ describe('JWTGenerator', () => {
 
     chai.expect(payload.name).to.be.equal('saviao')
     chai.expect(payload.sub).to.be.equal('9b1deb4d-3b7d-aaaa-bbbb-2b0d7b3dcb6d')
+  })
+
+  it('should return invalid token when token provided is invalid', async () => {
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+    const generator = new JWTGenerator()
+
+    try {
+      await generator.verifyToken(token)
+    } catch (error) {
+      chai.expect(error instanceof JsonWebTokenError).to.be.true
+    }
   })
 })
