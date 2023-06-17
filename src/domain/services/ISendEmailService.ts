eb947abf3ea601587d97a766487bd33fe2a90acc
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-17 18:19:46 -0300

    refactor ISendEmailService

--- src/domain/services/ISendEmailService.ts
+++ src/domain/services/ISendEmailService.ts
@@ -1,3 +1,6 @@
+import { type Transporter } from 'nodemailer'
+
 export interface ISendEmailService {
+  transporter: Transporter
   sendEmail: (...parameters: any[]) => Promise<void>
 }
--- src/infrastructure/services/ChangePasswordEmailService.ts
+++ src/infrastructure/services/ChangePasswordEmailService.ts
@@ -5,7 +5,7 @@ import path from 'path'
 import { EmailTransporter } from '../EmailTransporter'
 
 export class ChangePasswordEmailService implements ISendEmailService {
-  private readonly transporter = EmailTransporter.getTransporter()
+  transporter = EmailTransporter.getTransporter()
   async sendEmail(username: string, email: string): Promise<void> {
     const html = await readFile(path.resolve(__dirname, '../templates/changePasswordTemplate.html'), {
       encoding: 'utf8'
