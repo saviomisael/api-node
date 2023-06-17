a3c1f2459417a0465d1f07902aa31c04ed13d918
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-17 19:34:14 -0300

    refactor ChangePasswordEmailService

--- src/infrastructure/services/ChangePasswordEmailService.ts
+++ src/infrastructure/services/ChangePasswordEmailService.ts
@@ -4,12 +4,10 @@ import { SendEmailService } from './SendEmailService'
 export class ChangePasswordEmailService implements ISendEmailService {
   private readonly sendEmailService = new SendEmailService()
   async sendEmail(username: string, email: string): Promise<void> {
-    await Promise.all([
-      this.sendEmailService.setTemplate('changePassword', {
-        username,
-        date: `${new Date(new Date().toUTCString()).getTime()}`
-      }),
-      this.sendEmailService.sendEmail(email, `Alteração de Senha ${new Date().toUTCString()}`)
-    ])
+    await this.sendEmailService.setTemplate('changePassword', {
+      username,
+      date: `${new Date(new Date().toUTCString()).getTime()}`
+    })
+    await this.sendEmailService.sendEmail(email, `Alteração de Senha ${new Date().toUTCString()}`)
   }
 }
