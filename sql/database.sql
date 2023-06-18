ef4e050d0f40a49e4b5e4aabfe6eecbfe2c9a597
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-17 21:28:53 -0300

    add reviews table

--- sql/database.sql
+++ sql/database.sql
@@ -65,3 +65,14 @@ CREATE TABLE reviewers (
   INDEX email_idx (email)
 )
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE reviews (
+  id VARCHAR(36) PRIMARY KEY NOT NULL,
+  description TEXT NOT NULL,
+  stars SMALLINT NOT NULL,
+  fk_game VARCHAR(36) NOT NULL,
+  fk_reviewer VARCHAR(36) NOT NULL,
+  FOREIGN KEY (fk_game) REFERENCES games(id),
+  FOREIGN KEY (fk_reviewer) REFERENCES reviewers(id)
+)
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
