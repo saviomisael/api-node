3fa1820a93efe5b68cceedd740c6a8028bcc1b7d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-18 13:20:49 -0300

    add validations for description of review

--- src/presentation/dto/CreateReviewDTO.ts
+++ src/presentation/dto/CreateReviewDTO.ts
@@ -1,8 +1,15 @@
-import { Matches, Max, Min } from 'class-validator'
+import { Matches, Max, MaxLength, Min, MinLength } from 'class-validator'
 import { uuidRegex } from '../constants'
 
 export class CreateReviewDTO {
+  @MinLength(10, {
+    message: 'A review deve ter no mínimo 10 caracteres.'
+  })
+  @MaxLength(1000, {
+    message: 'A review deve ter no máximo 1000 caracteres.'
+  })
   description!: string
+
   @Min(1, {
     message: 'A review deve ter pelo menos 1 estrela.'
   })
