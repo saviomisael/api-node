54305c231229b2c17b9a3f9f3251958cf4250785
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-19 16:08:59 -0300

    refactor GameRepository

--- src/infrastructure/repositories/GameRepository.ts
+++ src/infrastructure/repositories/GameRepository.ts
@@ -1,276 +1,134 @@
-import { type Game } from '$/domain/entities'
+import { AgeRating, Game, Genre, Platform } from '$/domain/entities'
 import { Review } from '$/domain/entities/Review'
 import { type IGameRepository } from '$/domain/repositories'
 import { Owner } from '$/domain/value-objects/Owner'
 import { type Connection } from 'mysql2/promise'
+import { AppDataSource } from '../AppDataSource'
 import { DBConnection } from '../DBConnection'
 import { maxGamesPerPage } from '../constants'
-import { GameNotExistsError } from '../errors/GameNotExistsError'
-import { GameRowDataMapper } from '../mapper/GameRowDataMapper'
-import { type GameRowData } from '../row-data/GameRowData'
 import { type ReviewRowData } from '../row-data/ReviewRowData'
 
 export class GameRepository implements IGameRepository {
   private connection!: Connection
+  private readonly gameRepository = AppDataSource.getRepository(Game)
+  private readonly platformRepository = AppDataSource.getRepository(Platform)
+  private readonly genreRepository = AppDataSource.getRepository(Genre)
+  private readonly ageRepository = AppDataSource.getRepository(AgeRating)
 
-  async getById(gameId: string): Promise<Game | null> {
-    this.connection = await DBConnection.getConnection()
-
-    const results = await this.connection.execute(
-      `
-      SELECT g.id AS game_id,
-      g.name AS game_name,
-      g.description AS game_description,
-      g.price AS game_price,
-      g.releaseDate,
-      ar.id AS age_rating_id,
-      ar.age,
-      ar.description AS age_rating_description,
-      GROUP_CONCAT(DISTINCT gr.id) AS genre_ids,
-      GROUP_CONCAT(DISTINCT gr.name) AS genre_names,
-      GROUP_CONCAT(DISTINCT p.id) AS platform_ids,
-      GROUP_CONCAT(DISTINCT p.name) AS platform_names
-      FROM games AS g
-      JOIN ageRatings AS ar ON g.fkAgeRating = ar.id
-      JOIN games_genres AS gg ON gg.fk_game = g.id
-      JOIN genres AS gr ON gg.fk_genre = gr.id
-      JOIN games_platforms AS gp ON gp.fk_game = g.id
-      JOIN platforms AS p ON gp.fk_platform = p.id
-      WHERE g.id = ?
-      GROUP BY g.id
-    `,
-      [gameId]
-    )
-
-    const rows = results[0] as any[]
-
-    if (rows.length === 0) return null
-
-    const game = GameRowDataMapper.toEntity(rows[0] as GameRowData)
-
-    const reviews = await this.getReviewsByGame(gameId)
-
-    for (const review of reviews) {
-      game.addReview(review)
-    }
+  async getById(id: string): Promise<Game | null> {
+    // TODO get reviews
+    const game = await this.gameRepository.findOne({
+      where: { id },
+      relations: {
+        ageRating: true,
+        platforms: true,
+        genres: true
+      }
+    })
 
     return game
   }
 
   async create(game: Game): Promise<void> {
-    this.connection = await DBConnection.getConnection()
+    const platforms = []
+    const genres = []
 
-    const pipeline: Array<Promise<any>> = [
-      this.connection.execute(
-        `
-      INSERT INTO games (id, price, name, description, releaseDate, fkAgeRating)
-      VALUES (?, ?, ?, ?, ?, ?)
-    `,
-        [game.id, game.getPrice(), game.getName(), game.getDescription(), game.getReleaseDate(), game.getAgeRating().id]
-      )
-    ]
+    for (const platform of game.platforms) {
+      const platformFromDB = await this.platformRepository.findOne({ where: { id: platform.id } })
 
-    for (const platform of [...game.getPlatforms()]) {
-      pipeline.push(
-        this.connection.execute('INSERT INTO games_platforms (fk_platform, fk_game) VALUES (?, ?)', [
-          platform.id,
-          game.id
-        ])
-      )
+      if (platformFromDB != null) {
+        platforms.push(platformFromDB)
+      }
     }
 
-    for (const genre of [...game.getGenres()]) {
-      pipeline.push(
-        this.connection.execute('INSERT INTO games_genres (fk_genre, fk_game) VALUES (?, ?)', [genre.id, game.id])
-      )
-    }
+    for (const genre of game.genres) {
+      const genreFromDB = await this.platformRepository.findOne({ where: { id: genre.id } })
 
-    await Promise.all(pipeline)
+      if (genreFromDB != null) {
+        genres.push(genreFromDB)
+      }
+    }
+    const age = await this.ageRepository.findOne({ where: { id: game.ageRating.id } })
+
+    if (age != null) {
+      game.platforms = platforms
+      game.genres = genres
+      game.ageRating = age
+
+      await this.gameRepository.save(game)
+    }
   }
 
-  async updateGame(newGame: Game): Promise<void> {
-    this.connection = await DBConnection.getConnection()
-
-    const pipeline = [this.connection.execute('DELETE FROM games_platforms WHERE fk_game = ?', [newGame.id])]
-
-    for (const platform of [...newGame.getPlatforms()]) {
-      pipeline.push(
-        this.connection.execute('INSERT INTO games_platforms (fk_platform, fk_game) VALUES (?, ?)', [
-          platform.id,
-          newGame.id
-        ])
-      )
-    }
-    pipeline.push(this.connection.execute('DELETE FROM games_genres WHERE fk_game = ?', [newGame.id]))
-
-    for (const genre of [...newGame.getGenres()]) {
-      pipeline.push(
-        this.connection.execute('INSERT INTO games_genres (fk_genre, fk_game) VALUES (?, ?)', [genre.id, newGame.id])
-      )
-    }
-    pipeline.push(
-      this.connection.execute(
-        `
-      UPDATE games
-      SET price = ?,
-      name = ?,
-      description = ?,
-      releaseDate = ?,
-      fkAgeRating = ?
-      WHERE id = ?
-    `,
-        [
-          newGame.getPrice(),
-          newGame.getName(),
-          newGame.getDescription(),
-          newGame.getReleaseDate(),
-          newGame.getAgeRating().id,
-          newGame.id
-        ]
-      )
-    )
-
-    await Promise.all([...pipeline])
+  async updateGame(game: Game): Promise<void> {
+    await this.create(game)
   }
 
   async deleteGameById(gameId: string): Promise<void> {
-    this.connection = await DBConnection.getConnection()
-
-    const pipeline = [this.connection.execute('DELETE FROM games_platforms WHERE fk_game = ?', [gameId])]
-
-    pipeline.push(this.connection.execute('DELETE FROM games_genres WHERE fk_game = ?', [gameId]))
-
-    pipeline.push(this.connection.execute('DELETE FROM games WHERE id = ?', [gameId]))
-
-    pipeline.push(this.connection.execute('DELETE FROM reviews WHERE fk_game = ?', [gameId]))
-
-    await Promise.all([...pipeline])
+    await this.gameRepository.delete(gameId)
   }
 
   async getAll(page: number, sortType: 'releaseDate', sortOrder: 'ASC' | 'DESC'): Promise<Game[]> {
-    this.connection = await DBConnection.getConnection()
-
     const orders = {
-      releaseDate: 'DATE(g.releaseDate)'
+      releaseDate: {
+        releaseDate: sortOrder
+      }
     }
 
-    const query = `
-      SELECT g.id AS game_id,
-      g.name AS game_name,
-      g.description AS game_description,
-      g.price AS game_price,
-      g.releaseDate,
-      ar.id AS age_rating_id,
-      ar.age,
-      ar.description AS age_rating_description,
-      GROUP_CONCAT(DISTINCT gr.id) AS genre_ids,
-      GROUP_CONCAT(DISTINCT gr.name) AS genre_names,
-      GROUP_CONCAT(DISTINCT p.id) AS platform_ids,
-      GROUP_CONCAT(DISTINCT p.name) AS platform_names
-      FROM games AS g
-      JOIN ageRatings AS ar ON g.fkAgeRating = ar.id
-      JOIN games_genres AS gg ON gg.fk_game = g.id
-      JOIN genres AS gr ON gg.fk_genre = gr.id
-      JOIN games_platforms AS gp ON gp.fk_game = g.id
-      JOIN platforms AS p ON gp.fk_platform = p.id
-      GROUP BY g.id
-      ORDER BY ${orders[sortType]} ${sortOrder}
-      LIMIT ${page < 2 ? 0 : (page - 1) * maxGamesPerPage},${maxGamesPerPage}
-    `
-
-    const result = await this.connection.execute(query)
-
-    const rows = result[0] as GameRowData[]
-
-    return rows.map((x) => GameRowDataMapper.toEntity(x))
+    return await this.gameRepository.find({
+      relations: {
+        ageRating: true,
+        platforms: true,
+        genres: true
+      },
+      order: orders[sortType],
+      take: maxGamesPerPage,
+      skip: page < 2 ? 0 : (page - 1) * maxGamesPerPage
+    })
   }
 
   async searchByTerm(term: string, page: number, sortType: 'releaseDate', sortOrder: 'ASC' | 'DESC'): Promise<Game[]> {
-    this.connection = await DBConnection.getConnection()
-
     term = `+${term}`
 
     const orders = {
-      releaseDate: 'DATE(g.releaseDate)'
+      releaseDate: 'games_searched.releaseDate'
     }
 
-    const query = `
-      SELECT g.id
-      FROM games AS g
-      JOIN ageRatings AS ar ON g.fkAgeRating = ar.id
-      JOIN games_genres AS gg ON gg.fk_game = g.id
-      JOIN genres AS gr ON gg.fk_genre = gr.id
-      JOIN games_platforms AS gp ON gp.fk_game = g.id
-      JOIN platforms AS p ON gp.fk_platform = p.id
-      WHERE MATCH(gr.name) AGAINST (? IN BOOLEAN MODE)
-      OR MATCH(g.name) AGAINST (? IN BOOLEAN MODE)
-      OR MATCH(p.name) AGAINST (? IN BOOLEAN MODE)
-      GROUP BY g.id
-      ORDER BY ${orders[sortType]} ${sortOrder}
-      LIMIT ${page < 2 ? 0 : (page - 1) * maxGamesPerPage},${maxGamesPerPage}
-    `
-
-    const result = await this.connection.execute(query, [term, term, term])
-
-    const rows = result[0] as any[]
-
-    const pipeline: Array<Promise<Game | null>> = []
-
-    for (const row of rows) {
-      pipeline.push(this.getById(row.id as string))
-    }
-
-    const gamesPromise = await Promise.all([...pipeline])
-
-    const games: Game[] = []
-
-    for (const game of gamesPromise) {
-      if (game !== null) games.push(game)
-    }
-
-    const hasNull = gamesPromise.length !== games.length
-
-    if (hasNull) {
-      throw new GameNotExistsError()
-    }
-
-    return games
+    return await this.gameRepository
+      .createQueryBuilder('games_searched')
+      .innerJoinAndSelect('games.platforms', 'platforms')
+      .innerJoinAndSelect('games.genres', 'genres')
+      .innerJoinAndSelect('games.ageRating', 'age')
+      .select()
+      .where('MATCH(games_searched.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .orWhere('OR MATCH(platforms.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .orWhere('OR MATCH(genres.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .orderBy(`${orders[sortType]}`, sortOrder)
+      .skip(page < 2 ? 0 : (page - 1) * maxGamesPerPage)
+      .take(maxGamesPerPage)
+      .getMany()
   }
 
   async getMaxPagesBySearch(term: string): Promise<number> {
-    this.connection = await DBConnection.getConnection()
-
     term = `+${term}`
 
-    const result = await this.connection.execute(
-      `
-        SELECT COUNT(DISTINCT g.id) AS numGames
-        FROM games AS g
-        JOIN ageRatings AS ar ON g.fkAgeRating = ar.id
-        JOIN games_genres AS gg ON gg.fk_game = g.id
-        JOIN genres AS gr ON gg.fk_genre = gr.id
-        JOIN games_platforms AS gp ON gp.fk_game = g.id
-        JOIN platforms AS p ON gp.fk_platform = p.id
-        WHERE MATCH(gr.name) AGAINST (? IN BOOLEAN MODE)
-        OR MATCH(g.name) AGAINST (? IN BOOLEAN MODE)
-        OR MATCH(p.name) AGAINST (? IN BOOLEAN MODE)
-      `,
-      [term, term, term]
-    )
+    const count = await this.gameRepository
+      .createQueryBuilder('games_searched')
+      .innerJoinAndSelect('games.platforms', 'platforms')
+      .innerJoinAndSelect('games.genres', 'genres')
+      .innerJoinAndSelect('games.ageRating', 'age')
+      .select()
+      .where('MATCH(games_searched.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .orWhere('OR MATCH(platforms.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .orWhere('OR MATCH(genres.name) AGAINST (:query IN BOOLEAN MODE)', { query: term })
+      .getCount()
 
-    const rows = result[0] as any[]
-
-    return Math.ceil((rows[0].numGames as number) / maxGamesPerPage)
+    return Math.ceil(count / maxGamesPerPage)
   }
 
   async getMaxPages(): Promise<number> {
-    this.connection = await DBConnection.getConnection()
+    const count = await this.gameRepository.count()
 
-    const result = await this.connection.execute('SELECT COUNT(id) AS numGames FROM games')
-
-    const rows = result[0] as any[]
-
-    return Math.ceil((rows[0].numGames as number) / maxGamesPerPage)
+    return Math.ceil(count / maxGamesPerPage)
   }
 
   async createReview(review: Review): Promise<void> {
@@ -282,14 +140,10 @@ export class GameRepository implements IGameRepository {
     )
   }
 
-  async verifyGameAlreadyExists(gameId: string): Promise<boolean> {
-    this.connection = await DBConnection.getConnection()
+  async verifyGameAlreadyExists(id: string): Promise<boolean> {
+    const game = await this.gameRepository.findOne({ where: { id } })
 
-    const result = await this.connection.execute('SELECT id FROM games WHERE id = ?', [gameId])
-
-    const rows = result[0] as any[]
-
-    return rows.length > 0
+    return game != null
   }
 
   async updateReview(review: Review): Promise<void> {
