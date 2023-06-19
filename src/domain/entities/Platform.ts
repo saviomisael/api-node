a32d2c97f17319e76467601aa9c2c624278a4989
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-19 11:22:24 -0300

    refactor Platform entity

--- src/domain/entities/Platform.ts
+++ src/domain/entities/Platform.ts
@@ -1,5 +1,5 @@
 import { AggregateRoot } from '$/domain/entities/AggregateRoot'
-import { Column, Entity } from 'typeorm'
+import { Column, Entity, Index } from 'typeorm'
 
 @Entity('platforms')
 export class Platform extends AggregateRoot {
@@ -8,5 +8,6 @@ export class Platform extends AggregateRoot {
     unique: true,
     length: 256
   })
+  @Index('platform_name_idx', { synchronize: false })
   name!: string
 }
