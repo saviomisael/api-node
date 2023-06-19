102db7435fc6a27b1f2db925745ea7849a1e495d
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-19 17:12:25 -0300

    refactor GenreRepository

--- src/infrastructure/repositories/GenreRepository.ts
+++ src/infrastructure/repositories/GenreRepository.ts
@@ -1,19 +1,19 @@
-import { Genre } from '$/domain/entities'
+import { Game, Genre } from '$/domain/entities'
 import { type IGenreRepository } from '$/domain/repositories'
 import { type Connection } from 'mysql2/promise'
 import { AppDataSource } from '../AppDataSource'
-import { DBConnection } from '../DBConnection'
 
 export class GenreRepository implements IGenreRepository {
-  private connection!: Connection
-  private readonly repository = AppDataSource.getRepository(Genre)
+  private readonly connection!: Connection
+  private readonly genreRepository = AppDataSource.getRepository(Genre)
+  private readonly gameRepository = AppDataSource.getRepository(Game)
 
   async createGenre(genre: Genre): Promise<void> {
-    await this.repository.save(genre)
+    await this.genreRepository.save(genre)
   }
 
   async getGenreById(id: string): Promise<Genre | null> {
-    const genre = await this.repository.findOne({
+    const genre = await this.genreRepository.findOne({
       where: {
         id
       }
@@ -23,7 +23,7 @@ export class GenreRepository implements IGenreRepository {
   }
 
   async getGenreByName(name: string): Promise<Genre | null> {
-    return await this.repository.findOne({
+    return await this.genreRepository.findOne({
       where: {
         name
       }
@@ -31,7 +31,7 @@ export class GenreRepository implements IGenreRepository {
   }
 
   async getAll(): Promise<Genre[]> {
-    const genres = await this.repository.find()
+    const genres = await this.genreRepository.find()
 
     if (genres === undefined) return []
 
@@ -39,16 +39,12 @@ export class GenreRepository implements IGenreRepository {
   }
 
   async deleteGenreById(id: string): Promise<void> {
-    await this.repository.delete({ id })
+    await this.genreRepository.delete({ id })
   }
 
-  async hasRelatedGames(genreId: string): Promise<boolean> {
-    this.connection = await DBConnection.getConnection()
+  async hasRelatedGames(id: string): Promise<boolean> {
+    const games = await this.gameRepository.find({ where: { genres: { id } } })
 
-    const result = await this.connection.execute('SELECT fk_genre FROM games_genres WHERE fk_genre = ?', [genreId])
-
-    const rows = result[0] as any[]
-
-    return rows.length > 0
+    return games.length > 0
   }
 }
