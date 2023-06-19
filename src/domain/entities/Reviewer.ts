b753cb07a54c7ca6f6b22912c5d186bc92bf5596
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-19 12:18:41 -0300

    refactor Reviewer

--- src/domain/entities/Reviewer.ts
+++ src/domain/entities/Reviewer.ts
@@ -1,53 +1,46 @@
 import { newDateUtcTime } from '$/infrastructure/constants'
+import { Column, Entity, Index } from 'typeorm'
 import { addHoursHelper } from '../helpers/addHoursHelper'
 import { AggregateRoot } from './AggregateRoot'
 
+@Entity('reviewers')
 export class Reviewer extends AggregateRoot {
-  private temporaryPassword!: string
-  private tempPasswordTime!: Date | null
-  private createdAtUtcTime: Date
-  constructor(private readonly username: string, private readonly password: string, private readonly email: string) {
-    super()
-    this.createdAtUtcTime = newDateUtcTime()
-  }
+  @Column({
+    length: 60,
+    default: "''"
+  })
+  temporaryPassword!: string
 
-  setTemporaryPassword(password: string): void {
-    this.temporaryPassword = password
-  }
+  @Column({ type: 'datetime', nullable: true, default: 'null' })
+  tempPasswordTime!: Date | null
 
-  getTemporaryPassword(): string {
-    return this.temporaryPassword
-  }
+  @Column({
+    type: 'datetime',
+    nullable: false
+  })
+  createdAtUtcTime: Date = newDateUtcTime()
+
+  @Column({
+    length: 255,
+    nullable: false
+  })
+  @Index('username_reviewers_idx', { synchronize: false })
+  username!: string
+
+  @Column({
+    length: 60,
+    nullable: false
+  })
+  password!: string
+
+  @Column({
+    length: 255,
+    nullable: false
+  })
+  @Index('email_reviewers_idx', { synchronize: false })
+  email!: string
 
   generateTempPasswordTime(): void {
     this.tempPasswordTime = addHoursHelper(newDateUtcTime(), 1)
-  }
-
-  getTempPasswordTime(): Date | null {
-    return this.tempPasswordTime
-  }
-
-  setTempPasswordTime(date: Date | null): void {
-    this.tempPasswordTime = date
-  }
-
-  getUsername(): string {
-    return this.username
-  }
-
-  getPassword(): string {
-    return this.password
-  }
-
-  getCreatedAtUtcTime(): Date {
-    return this.createdAtUtcTime
-  }
-
-  setCreatedAtUtcTime(date: Date): void {
-    this.createdAtUtcTime = date
-  }
-
-  getEmail(): string {
-    return this.email
   }
 }
