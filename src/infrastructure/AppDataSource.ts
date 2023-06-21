8b38b28247842f02eb6800751894ea96c3896cc1
Savio Moreira <misaelmoreira99.sm@gmail.com>
2023-06-21 11:04:17 -0300

    refactor AppDataSource

--- src/infrastructure/AppDataSource.ts
+++ src/infrastructure/AppDataSource.ts
@@ -1,10 +1,10 @@
 import { AgeRating, Game, Genre, Platform, Review, Reviewer } from '$/domain/entities'
 import dotenv from 'dotenv'
-import { DataSource, type DataSourceOptions } from 'typeorm'
+import { DataSource } from 'typeorm'
 
 dotenv.config()
 
-let options: DataSourceOptions = {
+export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'postgres',
   port: 5432,
@@ -13,26 +13,5 @@ let options: DataSourceOptions = {
   database: 'gamesdb',
   logging: false,
   entities: [AgeRating, Genre, Platform, Reviewer, Game, Review],
-  extra: {
-    charset: 'utf8mb4_unicode_ci'
-  },
   migrations: ['./src/infrastructure/migrations/*.ts']
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  options = {
-    type: 'postgres',
-    host: 'postgres',
-    port: 5432,
-    username: process.env.PGUSER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: 'gamesdb',
-    synchronize: true,
-    logging: false,
-    entities: [AgeRating, Genre, Platform, Reviewer, Game, Review],
-    migrations: ['./src/infrastructure/migrations/*.ts']
-  }
-}
-export const AppDataSource = new DataSource({
-  ...options
 })
